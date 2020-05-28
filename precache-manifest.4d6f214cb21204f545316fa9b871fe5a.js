425dfd94af5827a0b94b39cdb9ff7037585bfdf9
nn <rohan.rawat.9897@gmail.com>
2020-05-28 12:44:40 +0530

    Updates

---
+++ precache-manifest.4d6f214cb21204f545316fa9b871fe5a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a3653833bed80aec41183c10429e90",
+    "url": "/rapp/index.html"
+  },
+  {
+    "revision": "dd9d6f2c9def31791c2f",
+    "url": "/rapp/static/css/main.918326aa.chunk.css"
+  },
+  {
+    "revision": "4451206e91d49649412e",
+    "url": "/rapp/static/js/2.8e98beff.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/rapp/static/js/2.8e98beff.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd9d6f2c9def31791c2f",
+    "url": "/rapp/static/js/main.eb3af911.chunk.js"
+  },
+  {
+    "revision": "421459c1fb5c8f42d822",
+    "url": "/rapp/static/js/runtime-main.c08ae36d.js"
+  }
+]);
